b471ccef1d5db3837f0529c670cc66bd1a22be4c
Sembrador <36065369+fmolero@users.noreply.github.com>
2021-01-21 23:46:56 +0100

    Update ree.php

--- desktop/php/ree.php
+++ desktop/php/ree.php
@@ -119,13 +119,17 @@ foreach (jeeObject::all() as $object) {
 
             </div>
      <div class="form-group">
-              <label class="col-sm-3 control-label">{{Hora Cambio Tarifa}}</label>
+
+              <label class="col-sm-3 control-label">{{Hora Cambio Día}}</label>
               <div class="col-sm-3">
-		<input class="form-control eqLogicAttr" data-l1key="configuration" data-l2key="hora">
-	      </div>
+                <select id="sel_hora" class="form-control eqLogicAttr" data-l1key="configuration" data-l2key="hora">
+                  <option value="21:59:00">{{22 horas}}</option>
+                  <option value="22:59:00">{{23 horas}}</option>
+                  <option value="23:59:00">{{00 horas}}</option>
+		 </select>
+              </div>
 
-        </div>
-
+            </div>
 </fieldset>
 </form>
 </div>
