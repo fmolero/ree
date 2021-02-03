99b3ca151f867391fcb192c7dfefa453c142f30d
Sembrador <36065369+fmolero@users.noreply.github.com>
2021-02-03 10:07:14 +0100

    Update ree.class.php

--- core/class/ree.class.php
+++ core/class/ree.class.php
@@ -347,15 +347,10 @@ class reeCmd extends cmd {
 		log::add('ree', 'debug', 'Datos json: ' . print_r($response, True));
                // On rÃ©cupÃ¨re l'Ã©quipement Ã  partir de l'identifiant fournit par la commande
                $reeObj = ree::byId($this->getEqlogic_id());
-               //Objeto para manejar los datos de gráfico
-		$dataGraph = $reeObj->getCmd('info', 'graph');
-		$graph = "Prueba";
-		//grabamos los datos de gráfico
-	    	$dataGraph->event($graph);
-	    	$dataGraph->save();
+               
 		// On rÃ©cupÃ¨re la commande 'data' appartenant Ã  l'Ã©quipement			
                for ($x = 0; $x <= 23; $x++) {
-		   $graph = $graph . "1,";
+		   //$graph = $graph . "1,";
                    $dataCmd = $reeObj->getCmd('info', self::$_horas2[$x]);
                    $dataCmd->event($obj['indicator']['values'][$x]['value'] / 1000);
                    //$dataCmd->event($globalp1);
@@ -366,7 +361,15 @@ class reeCmd extends cmd {
 	    	//$dataGraph->event($graph);
 	    	//$dataGraph->save();
                }
-		$dataCmd = $reeObj->getCmd('info', 'name');
+		
+	    	//Objeto para manejar los datos de gráfico
+		$dataCmd = $reeObj->getCmd('info', 'graph');
+		$graph = "Prueba";
+		//grabamos los datos de gráfico
+	    	$dataCmd->event($graph);
+	    	$dataCmd->save();
+	    
+	    	$dataCmd = $reeObj->getCmd('info', 'name');
                    $dataCmd->event($obj['indicator']['short_name']);
                    //$dataCmd->event($globalp1);
                    $dataCmd->save();
