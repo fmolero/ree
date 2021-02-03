a3630cda4db3caca4a1d1083a63210d23692ecd4
Sembrador <36065369+fmolero@users.noreply.github.com>
2021-02-03 13:11:40 +0100

    Update ree.class.php

--- core/class/ree.class.php
+++ core/class/ree.class.php
@@ -363,6 +363,7 @@ class reeCmd extends cmd {
                }
 		
 	    	$dataCmd = $reeObj->getCmd('info', 'graph');
+	    	   $graph = $graph . ($obj['indicator']['short_name']) . "," . ($obj['indicator']['date']);
                    $dataCmd->event($graph);
                    //$dataCmd->event($globalp1);
                    $dataCmd->save();
