cb6d58beb997354e8e3692b4bc43117dd8122cf2
Sembrador <36065369+fmolero@users.noreply.github.com>
2021-02-03 09:37:02 +0100

    Update ree.class.php

--- core/class/ree.class.php
+++ core/class/ree.class.php
@@ -352,17 +352,19 @@ class reeCmd extends cmd {
 		$graph = "";
                // On rÃ©cupÃ¨re la commande 'data' appartenant Ã  l'Ã©quipement
                for ($x = 0; $x <= 23; $x++) {
-                   $graph = $graph . ($obj['indicator']['values'][$x]['value'] / 1000) . ", ";
+                   //$graph = $graph . ($obj['indicator']['values'][$x]['value'] / 1000) . ", ";
+		   $graph = $obj['indicator']['values'][$x]['value'] / 1000;
 		   $dataCmd = $reeObj->getCmd('info', self::$_horas2[$x]);
                    $dataCmd->event($obj['indicator']['values'][$x]['value'] / 1000);
                    //$dataCmd->event($globalp1);
                    $dataCmd->save();
 			$tarifa[] = $obj['indicator']['values'][$x]['value'] / 1000;
                    }
-               }
-	    	//grabamos los datos de gráfico
-	    	$datagraph->event($graph);
-	    	$datagraph->save();
+               //grabamos los datos de gráfico
+	    	$dataGraph->event($graph);
+	    	$dataGraph->save();
+		}
+	    	
 	    
 		$dataCmd = $reeObj->getCmd('info', 'name');
                    $dataCmd->event($obj['indicator']['short_name']);
